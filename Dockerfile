ac351a85eab71f328e590af43f67ba199845d9d9
C. Annable <christopher.annable@gmail.com>
2018-02-09 18:56:14 -0500

    Add dumb-init.

--- Dockerfile
+++ Dockerfile
@@ -6,5 +6,6 @@ VOLUME ["/var/lib/postgresql"]
 VOLUME ["/root"]
 COPY ["./data", "/data"]
 RUN ["/bin/sh", "/data/build.sh"]
+ENTRYPOINT ["/usr/bin/dumb-init", "--"]
 CMD ["/bin/sh", "/data/init.sh"]
 
--- data/build.sh
+++ data/build.sh
@@ -2,7 +2,7 @@
 
 echo Installing Metasploit Framework
 apt-get update
-apt-get -y install metasploit-framework vim
+apt-get -y install dumb-init metasploit-framework vim
 
 # Purge Apt Caches
 apt-get -y clean
